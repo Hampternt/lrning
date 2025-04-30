e8012f9a0b6a64e90624f4a567f697eecc2e5330
Hampternt <jesperlovland@proton.me>
2025-04-30 18:38:11 +0200

    more add's

--- src/guessing_game.rs
+++ src/guessing_game.rs
@@ -1,11 +1,20 @@
 use std::io::stdin;
 use crate::get_input;
-use rand;
+use rand::Rng;
 
 pub fn guessing_game() {
     println!("Guess a number:");
 
-    let guess = get_input!();
+    let guess: i32 = get_input!().trim().parse().expect("You are using the wrong numba bish");
 
     println!("You guessed {}", guess);
+
+    let random_number = rand::thread_rng().gen_range(1..=100);
+    println!("{}", random_number);
+    match guess {
+        1 => {
+            println!("You guessed a too small numba bish.");
+        }, 
+        _ => println!("wrong and everything else.")
+    }
 }
